38b39badaa6acf2e0794dd35c1ee76e5c2476344
Joshua Doharman Sinaga 251401058 <vivovplus090@gmail.com>
2026-09-10 12:07:11 +0700

    Pertemuan 1: Array dan Dynamic Array

---
+++ Pertemuan 1/array_dinamic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cout << "Masukkan jumlah elemen array: "; cin >> n;
+
+    int *arr = new int[n];
+    cout << "Masukkan " << n << " angka: ";
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];;
+    }
+
+    cout << "Isi array: ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    
+    delete [] arr;
+    return 0;
+}
